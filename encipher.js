0d814b580cea3ee977b80f3562c01f0f6c3dc441
Varun Haralalka <111225388+VarunHaralalka@users.noreply.github.com>
2022-09-02 00:18:32 +0530

    Add files via upload

---
+++ encipher.js
@@ -0,0 +1,52 @@
+window.onload = ()=>{
+    let buttton=document.querySelector("#btn");
+    buttton.addEventListener("click",ciphertxt);
+}
+
+function ciphertxt(){
+    var input = "";
+        let result =document.querySelector("#result");
+        var en="";
+        var i = 0;
+        var l = 0;
+        var c1 = 0;
+        var c2 = 0;
+        var x = ' ';
+        input=document.getElementById("Password").value;
+        c1=document.getElementById("key").value;
+        console.log(input)
+        console.log(c1)
+        c2 = c1 + 1;
+        l = input.length;
+        for (i = 0; i < l; i++)
+        {
+            x = input.charAt(i);
+            if (x.charAt(0) >= 65 && x.charAt(0) <= 90)
+            {
+                if (x.charAt(0) > (90 - c1))
+                {
+                    en += String.fromCharCode((64 + c1 - (90 - x.charAt(0))));
+                }
+                else 
+                {
+                    en += String.fromCharCode((x.charAt(0) + c1));
+                }
+            }
+            else if (x.charAt(0) >= 97 && x.charAt(0) <= 122)
+            {
+                if (x.charAt(0) > (122 - c2))
+                {
+                    en += String.fromCharCode((96 + c2 - (122 - x.charAt(0))));
+                }
+                else 
+                {
+                    en += String.fromCharCode((x.charAt(0) + c2));
+                }
+            }
+            else 
+            {
+                en += x;
+            }
+        }
+        document.getElementById("result").value=en;
+}
